8bedb4d40946c985eda0ad9d509cbcf20cab46e0
salmon9 <49511344+salmon9@users.noreply.github.com>
2021-04-30 14:26:23 +0900

    Update circle_rnn.py

--- circle_rnn.py
+++ circle_rnn.py
@@ -4,97 +4,77 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 
 
-class Model (nn.Module):
-  def __init__(self):
-    super(Model, self).__init__()
-    self.l1 = nn.Linear(6,60)
-    self.l2 = nn.Linear(60,120)
-    self.l3 = nn.Linear(120,6)
-
-  def forward(self,x):
-    x = F.relu(self.l1(x))
-    x = F.relu(self.l2(x))
-    x = self.l3(x)
-    return x
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.rnn = torch.nn.RNN(2, 20)
+        self.fc = torch.nn.Linear(20, 2)
+    def forward(self, x, hidden):
+    
+        output, h = self.rnn(x, hidden)
+        output = self.fc(output)
+        return output, h
 
 class my_dataset(torch.utils.data.Dataset):
   def __init__(self, point, noise):
 
     self.point = point
-    self.len = len(point)  #np.arrayならpoint.shape[0]でも可
+    self.len = len(point) 
 
   def __getitem__(self, index):
 
-    input = self.point[index-1]  #indexの扱いに気を付ける
-    target = self.point[index]  #最初と同じ点にならないように
+#      input = self.point[index-1] + np.random.normal(scale=0.001) #indexの扱いに気を付ける
+#      target = self.point[index]  + np.random.normal(scale=0.001) #最初と同じ点にならないように
 
-    return input, target
+      input = self.point[:-1] + np.random.normal(scale=0.001)
+      target = self.point[1:] + np.random.normal(scale=0.001)
+
+      print("input:", input.shape)
+      print("target:", target.shape)
+
+      return input, target
 
   def __len__(self):
     return self.len
 
 def get_dataset(n):
 
-    for i in range (1,5):
-      if i == 1:
+    parameter = np.radians(np.linspace(0,360,n)) #0〜360のランダム値 p:媒介変数
+    x = np.cos(parameter)
+    y = np.sin(parameter)
 
-        parameter = np.radians(np.linspace(1, 90, int(n/4)))
-        x_1 = 2*np.sin(parameter) + np.random.normal(scale=0.001)
-        y_1 = np.sin(2*parameter) + np.random.normal(scale=0.001)
-        dataset_1 = np.array([x_1, y_1, np.ones(int(n/4)), np.zeros(int(n/4)), np.zeros(int(n/4)), np.zeros(int(n/4))]) #(x,y,1,0,0,0)
-
-      elif i == 2:
-
-        parameter = np.radians(np.linspace(91, 180, int(n/4)))
-        x_2 = 2*np.sin(parameter) + np.random.normal(scale=0.001)
-        y_2 = np.sin(2*parameter) + np.random.normal(scale=0.001)
-        dataset_2 = np.array([x_2, y_2, np.zeros(int(n/4)), np.zeros(int(n/4)), np.zeros(int(n/4)), np.ones(int(n/4))]) #(x,y,0,0,0,1)
-
-      elif i == 3:
-
-        parameter = np.radians(np.linspace(181, 270, int(n/4)))
-        x_3 = 2*np.sin(parameter) + np.random.normal(scale=0.001)
-        y_3 = np.sin(2*parameter) + np.random.normal(scale=0.001)     
-        dataset_3 = np.array([x_3, y_3, np.zeros(int(n/4)), np.ones(int(n/4)), np.zeros(int(n/4)), np.zeros(int(n/4))]) #(x,y,0,1,0,0)
-
-      else:
-
-        parameter = np.radians(np.linspace(271, 360, int(n/4)))
-        x_4 = 2*np.sin(parameter) + np.random.normal(scale=0.001)
-        y_4 = np.sin(2*parameter) + np.random.normal(scale=0.001)
-        dataset_4 = np.array([x_4, y_4, np.zeros(int(n/4)), np.zeros(int(n/4)), np.ones(int(n/4)), np.zeros(int(n/4))]) #(x,y,0,0,1,0)
-
-    dataset = np.concatenate([dataset_1, dataset_2, dataset_3, dataset_4], 1)
-    x = np.concatenate([x_1, x_2, x_3, x_4])
-    y = np.concatenate([y_1, y_2, y_3, y_4])
-
-    dataset = dataset.T
-
-    print(dataset)
+    dataset = np.array([x,y]).T
 
     return dataset, x, y
 
 def train(data_loader, model, criterion, optimizer, epoch):
     
     train_loss_list = []
+    print("hello")
+
+    for batch_data in data_loader:
+        input, target = batch_data  
+        print("good bye")
+        hidden = torch.zeros(1, 99, 20) #(num_layers, num_batch, hidden_size)
     for t in range(epoch):
 
         train_loss = 0
-        for batch_data in data_loader: # 1ミニバッチずつ計算
+#        for batch_data in data_loader: # 1ミニバッチずつ計算
+            
+        model.zero_grad()
 
-            input, target = batch_data
-            optimizer.zero_grad()
+        output, hidden = model(input.float(), hidden.float())
+        loss = criterion(output, target.float())
 
-            output = model(input.float())
+        loss.backward()
 
-            loss = criterion(output, target.float())
-            loss.backward()
+        optimizer.step()
 
-            optimizer.step()
-            train_loss += loss.item()
-            print(t, loss.item())
+        train_loss += loss.item()
+        print(t, loss.item())
         ave_train_loss = train_loss/len(data_loader.dataset)
         train_loss_list.append(ave_train_loss)
 
@@ -104,12 +84,13 @@ def train(data_loader, model, criterion, optimizer, epoch):
 
 def test(first_point, model):
 
-    record_output = np.array([[0.0, 0.0, 1, 0, 0, 0]])
+    record_output = np.array([[1.0, 0.0]])
 
-    for i in range(360):
+    for i in range(1100):
+
         test_output = model(first_point)
         first_point = test_output
-        test_output = test_output.reshape(1,6).detach().numpy()
+        test_output = test_output.reshape(1,2).detach().numpy()
         record_output = np.concatenate(([record_output, test_output]), axis=0)
 
     return record_output
@@ -123,12 +104,12 @@ def drawing_loss_graph(epoch, train_loss_list):
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.grid()
-    plt.show()
-
+    plt.show()    
+    
 def main():
 
-    data_number = 360 #準備するデータの数
-    batch_size  = 90 # 1つのミニバッチのデータの数
+    data_number = 100 #準備するデータの数
+    batch_size  = 300 # 1つのミニバッチのデータの数
 
     points, x, y = get_dataset(data_number)
     dataset = my_dataset(points, noise=True)
@@ -139,21 +120,19 @@ def main():
     learning_rate = 1e-4
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    loss_log = [] # 学習状況のプロット用
     epoch = 200
 
-    first_point = [[0.0, 0.0, 1, 0, 0, 0]]
+    first_point = [[1.0,0.0]]
     first_point = torch.Tensor(first_point)
-
+        
     model.train()
     train(data_loader, model, criterion, optimizer, epoch)
 
     model.eval()
     final_test_output = test(first_point, model)
 
-    final_x, final_y, a, b, c, d = final_test_output.T  #出力にa,b,c,d は使わない(象限)
-
-    print("go")
+    final_x, final_y = final_test_output.T
+    ims = final_test_output.T
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -161,7 +140,10 @@ def main():
 
     plt.plot(x, y, label="original", color = "y")
     plt.scatter(final_x, final_y, label="trained", color = "b")
+
+
+    ani = animation.ArtistAnimation(fig, ims, interval=100)
+    plt.show()
+
 if __name__ == '__main__':
     main()
-
-    
