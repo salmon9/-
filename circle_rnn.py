eb9792ebf06c90e4a42d98afbaf428fe7b38b574
salmon9 <49511344+salmon9@users.noreply.github.com>
2021-05-28 16:52:03 +0900

    Update circle_rnn.py

--- circle_rnn.py
+++ circle_rnn.py
@@ -51,12 +51,14 @@ def get_dataset(n):
     y = np.sin(parameter)
 
     dataset = np.array([x,y]).T
+    test_points = dataset[0:20]
 
-    return dataset, x, y
+    return dataset, test_points
 
 def train (data_loader, model, criterion, optimizer, epoch):
 
     for i in range(epoch):
+
 
       train_loss = 0
       print("epoch:", i)
@@ -80,45 +82,14 @@ def train (data_loader, model, criterion, optimizer, epoch):
           print("ave:" ,ave_train_loss)
           print()
         """
-"""
-def train(data_loader, model, criterion, optimizer, epoch):
 
-    train_loss_list = []
-    print("hello")
-
-    for batch_data in data_loader:
-        input_data, target = batch_data
-        print("good bye")
-        hidden = torch.zeros(1, 99, 20) #(num_layers, num_batch, hidden_size)
-    for t in range(epoch):
-
-        train_loss = 0
-
-        model.zero_grad()
-
-        output, hidden = model(input_data.float(), hidden.float())
-        loss = criterion(output, target.float())
-
-        loss.backward()
-
-        optimizer.step()
-
-        train_loss += loss.item()
-        print(t, loss.item())
-        ave_train_loss = train_loss/len(data_loader.dataset)
-        train_loss_list.append(ave_train_loss)
-
-        print("ave:" ,ave_train_loss)
-        print()
-    drawing_loss_graph(epoch, train_loss_list)
-"""
 def test(first_point, model):
 
     record_output = np.array([[1.0, 0.0]])
 
-    for i in range(1100):
-
-        test_output = model(first_point)
+    for i in range(100):
+        hidden = torch.zeros(20, 20)
+        test_output = model(first_point, hidden.float())
         first_point = test_output
         test_output = test_output.reshape(1,2).detach().numpy()
         record_output = np.concatenate(([record_output, test_output]), axis=0)
@@ -140,15 +111,17 @@ def main():
 
     data_number = 100 #準備するデータの数
     batch_size  = 20 # 1つのミニバッチのデータの数
-    epoch = 70  # epoch数
+    epoch = 100  # epoch数
     time_step = batch_size
 
     input_size = 2
     hidden_size = 20
 
-    points, x, y = get_dataset(data_number)
+    points, test_points = get_dataset(data_number)
     dataset = my_dataset(points, time_step)
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=8)
+
+    print(test_points)
 
     model = Model(input_size, hidden_size)
     criterion = nn.MSELoss()
@@ -162,7 +135,7 @@ def main():
     train(data_loader, model, criterion, optimizer, epoch)
 
     model.eval()
-    final_test_output = test(first_point, model)
+    final_test_output = test(test_points, model)
 
     final_x, final_y = final_test_output.T
     ims = final_test_output.T
