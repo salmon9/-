91c3f0e520f962a7259313b733f5de7bd5d7bb6d
salmon9 <49511344+salmon9@users.noreply.github.com>
2021-06-21 14:49:22 +0900

    Update autoencoder_cnn_coco.py

--- autoencoder_cnn_coco.py
+++ autoencoder_cnn_coco.py
@@ -43,7 +43,8 @@ class Decoder(torch.nn.Module):
         x = torch.relu(self.fc3(x))
         x = torch.relu(self.fc4(x))
         x = torch.relu(self.fc5(x))
-        x = torch.tanh(self.fc6(x))  # -1～1に変換 sigmoid-> 0~1
+#        x = torch.tanh(self.fc6(x))  # -1～1に変換 sigmoid-> 0~1
+        x = self.fc6(x)
         return x
 
 class AutoEncoder(torch.nn.Module):
@@ -59,23 +60,22 @@ class AutoEncoder(torch.nn.Module):
 #if using nn.maxpool2d, reverse is nn.MaxUnpool2d
 
 class ImageFolder(Dataset):
-    IMG_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp"]
+    IMG_EXTENSIONS = [".jpg"]  #拡張子指定
 
     def __init__(self, img_dir, transform=None):
         # 画像ファイルのパス一覧を取得する。
-        self.img_paths = self._get_img_paths(img_dir)
-        self.transform = transform
-        img_list = []
+        self.img_paths = self._get_img_paths(img_dir)  #画像のディレクトリを入力
+        self.transform = transform  #transformの定義
+        img_list = []  #返す画像のデータセットの宣言
         for path in self.img_paths:
-            img = Image.open(path)
-            img = self.transform(img)
-            if img.shape[0]==1:
-                continue
-            img_list.append(img)
+            img = Image.open(path)  #画像読み込み
+            img = self.transform(img)  #画像処理
+            if img.shape[0]==1:  #(channel, height, width)のうちchannel=1(monocolour)を弾く
+                continue  #channel==1の時この後の行は実行されない＝appendされない
+            img_list.append(img)  #弾いた写真以外をappend
         self.img_list = img_list
-        self.length = len(self.img_list)
+        self.length = len(self.img_list)  #長さは返すデータセットのもの、元本のではない
     def __getitem__(self, index):
-
 
         return self.img_list[index]
 
@@ -95,53 +95,67 @@ class ImageFolder(Dataset):
 
 def get_dataset(batch_size):
 
-    size = (256, 256)
+    size = 256
     # Transform を作成する
-    transform = transforms.Compose([transforms.Resize(size), transforms.ToTensor()])
+    #Composeは［ ］の中を順番に 実行
+    transform = transforms.Compose([transforms.RandomResizedCrop(size), transforms.ToTensor()])
+    #RandomResizedCropは(size,size)の大きさにcropとresizeしてくれる
+    #size=(height,width)も可　
+    #ToTensorは(height, width, channel)を(channel, height, width)に変えてくれる
+    #元々のデータはndarray
+
     # Dataset を作成する
-    trainset = ImageFolder("train2014", transform)
-    testset = ImageFolder("test2014", transform)
+    trainset = ImageFolder("train2014_small", transform)
+    testset = ImageFolder("test2014_small", transform)
     # DataLoader を作成する
     trainloader = DataLoader(trainset, batch_size=batch_size, num_workers=2)
     testloader = DataLoader(testset, batch_size=batch_size, num_workers=2)
-
+    #batch_sizeは一回の学習になんこのデータを使うか
+    #num_workersは複数処理をするかどうか
     return trainloader, testloader
 
 def imshow(img, title):
     img = torchvision.utils.make_grid(img)
-    img = img / 2 + 0.5
+    #複数の画像を一枚に合成する関数
+    #入力tensor,出力tensor
+    print(img.shape)
+    img = img / 2 + 0.5 #色彩調整？？
+    print(img.shape)
     npimg = img.detach().numpy()
+    #tensorからndarrayに変換
+    #ndarrayじゃないとimshowで出力できない
     plt.imshow(np.transpose(npimg, (1, 2, 0)), )
+    #(channel, height, width)→(height, width, channel)
     plt.title(title)
     plt.show()
 #    plt.savefig('figure.png') # -----(2)
 
 def train(net, criterion, optimizer, epochs, trainloader):
-    losses = []
-    output_and_label = []
+    losses = []  #lossを入れる配列
+    output_and_label = []  #ouputとinputを一緒に格納する配列
 
-    for epoch in range(1, epochs+1):
+    for epoch in range(1, epochs+1):  #epoch回す
         print(f'epoch: {epoch}, ', end='')
-        running_loss = 0.0
-        for counter, img in enumerate(trainloader, 1):
-            optimizer.zero_grad()
-#            img = img.reshape(-1, input_size)
-            output = net(img)
-            loss = criterion(output, img)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-        avg_loss = running_loss / counter
-        losses.append(avg_loss)
-        print('loss:', avg_loss)
-        output_and_label.append((output, img))
+        running_loss = 0.0  #epoch毎にlossを0にリセット
+        for counter, img in enumerate(trainloader):  #インデックス、要素を取り出す
+            optimizer.zero_grad()  #最適化対象のすべてのパラメータの勾配を0にする
+#            img = img.reshape(-1, input_size)  #cnnだといらない
+            output = net(img)  #学習モデルにimgを入力、出力結果がoutput
+            loss = criterion(output, img)  #outputとinputを比較してlossを取得
+            loss.backward()  #損失関数の逆伝播
+            optimizer.step()  #重みの更新
+            running_loss += loss.item  #epoch毎のlossを計算したいからbatch毎に加算
+        avg_loss = running_loss / counter  #毎epochの平均loss
+        losses.append(avg_loss)  #matplotlibでlossの変化のグラフを出すために使用
+        print('loss:', avg_loss)  #lossの表示
+        output_and_label.append((output, img))  #outputとinputを一緒に格納
     print('finished')
     return output_and_label, losses
 
 def main():
     batch_size = 50
 
-    trainloader, testloader = get_dataset(batch_size)
+    trainloader, testloader = get_dataset(batch_size)  #train用とtest用のdatasetを作成
 
     iterator = iter(trainloader)
     x = next(iterator)
@@ -149,8 +163,8 @@ def main():
 
     net = AutoEncoder()
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.1)
-    EPOCHS = 10
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
+    EPOCHS = 20
 
     output_and_label, losses = train(net, criterion, optimizer, EPOCHS, trainloader)
 
