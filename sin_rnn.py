500e62628042865fe65b77e3affce2668089df3c
salmon9 <49511344+salmon9@users.noreply.github.com>
2021-07-09 16:27:03 +0900

    Update sin_rnn.py

--- sin_rnn.py
+++ sin_rnn.py
@@ -12,24 +12,24 @@ class Model(torch.nn.Module):
         super().__init__()
         self.rnn = torch.nn.RNN(input_size, hidden_size)
         self.fc = torch.nn.Linear(hidden_size, 1)
-    def forward(self, x, hidden):
+        self.hidden_size = hidden_size
+    def forward(self, x, hidden=None):
+        if hidden == None:
+            hidden = torch.zeros(x.shape[0], hidden_size)
         output, h = self.rnn(x, hidden)
-        output = self.fc(output[:, -1])
+        output = self.fc(output)
         return output, h
 
 class my_dataset(torch.utils.data.Dataset):
-    def __init__(self, points, time_step):
+    def __init__(self, points):
 
-        self.time_step = time_step
         self.points = points
         self.len = len(points)
 
     def __getitem__(self, index):
 
-        input_data = np.zeros((self.time_step, 2))
-        for i in range(self.time_step):
-            input_data[i] = self.points[index-self.time_step+i] + np.random.normal(scale=0.001)
-        target = self.points[index] + np.random.normal(scale=0.001)
+        input_data = self.points[index] + np.random.normal(scale=0.001)
+        target = self.points[index]
 
         return input_data, target
 
@@ -38,23 +38,33 @@ class my_dataset(torch.utils.data.Dataset):
 
 def get_dataset():
 
-    dataset = np.array([(0,0)])
+    dataset = np.array([0])
 
     for t in range (1, 1081):
 
         if t<361:
             x = np.sin(np.radians(t))
-            dataset = np.append(dataset, np.array([(t,x)]), axis=0)
+            dataset = np.append(dataset, np.array([x]), axis=0)
         elif t < 721:
             x = 3*np.sin(np.radians(t))
-            dataset = np.append(dataset, np.array([(t,x)]), axis=0)
+            dataset = np.append(dataset, np.array([x]), axis=0)
         else:
             x = 5*np.sin(np.radians(t))
-            dataset = np.append(dataset, np.array([(t,x)]), axis=0)
+            dataset = np.append(dataset, np.array([x]), axis=0)
 
     print("dataset shape:", dataset.shape)
     print("dataset type:", type(dataset))
+
     return dataset
+
+def dataset_mizumashi(dataset, seq_num):
+
+    new_dataset = dataset
+    for i in range(seq_num):
+        temp_array = dataset+i
+        new_dataset = np.vstack((new_dataset, temp_array))
+
+    return new_dataset
 
 def train (data_loader, model, criterion, optimizer, epoch):
 
@@ -68,23 +78,23 @@ def train (data_loader, model, criterion, optimizer, epoch):
             model.zero_grad()
 
             hidden = torch.zeros(1, 360, 20)
-            output, hidden = model(input_data.float(), hidden.float())
-            loss = criterion(output, target.float())
+            output, hidden = model(input_data[:,:-1].float(), hidden.float())
+            loss = criterion(output, target[:,1:].float())
             loss.backward()
             optimizer.step()
 
             print(j, ":", loss.item())
-
+            """
             train_loss += loss.item()
             ave_train_loss = train_loss/len(data_loader.dataset)
             train_loss_list.append(ave_train_loss)
             print("ave:" ,ave_train_loss)
             print()
-
+            """
 
 def test(test_points, model):
 
-    record_output = np.array([[0.0, 0.0]])
+    record_output = np.array([0])
 
     for i in range(100):
         hidden = torch.zeros(20, 20)
@@ -108,15 +118,21 @@ def drawing_loss_graph(epoch, train_loss_list):
 
 def main():
 
-    batch_size  = 360 # 1つのミニバッチのデータの数
+    batch_size  = 10 # 1つのミニバッチのデータの数
     epoch = 100  # epoch数
-    time_step = batch_size
 
     input_size = 2
     hidden_size = 20
 
+    seq_num = 100
+
     points= get_dataset()
-    dataset = my_dataset(points, time_step)
+
+    points = dataset_mizumashi(points, seq_num)
+
+    print(points.shape)
+    dataset = my_dataset(points)
+
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=6)
 
     """
@@ -135,7 +151,7 @@ def main():
     learning_rate = 1e-4
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    first_point = [[0.0,0.0]]
+    first_point = [0]
     first_point = torch.Tensor(first_point)
 
     model.train()
