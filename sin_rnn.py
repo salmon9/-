2ebdb9da2f2f6502bccb73b10c3dc54ea944c03b
salmon9 <49511344+salmon9@users.noreply.github.com>
2021-07-30 13:55:07 +0900

    Update sin_rnn.py

--- sin_rnn.py
+++ sin_rnn.py
@@ -38,70 +38,74 @@ class my_dataset(torch.utils.data.Dataset):
 
 def get_dataset():
 
-    dataset = np.array([0])
+    dataset = np.array([[[0]]])
 
     for t in range (1, 1081):
 
         if t<361:
             x = np.sin(np.radians(t))
-            dataset = np.append(dataset, np.array([x]), axis=0)
+            dataset = np.append(dataset, np.array([[[x]]]), axis=0)
         elif t < 721:
             x = 3*np.sin(np.radians(t))
-            dataset = np.append(dataset, np.array([x]), axis=0)
+            dataset = np.append(dataset, np.array([[[x]]]), axis=0)
         else:
             x = 5*np.sin(np.radians(t))
-            dataset = np.append(dataset, np.array([x]), axis=0)
+            dataset = np.append(dataset, np.array([[[x]]]), axis=0)
 
     print("dataset shape:", dataset.shape)
     print("dataset type:", type(dataset))
+    print(dataset)
 
     return dataset
 
 def dataset_mizumashi(dataset, seq_num):
 
     new_dataset = dataset
-    for i in range(seq_num):
+    for i in range(1,seq_num):
         temp_array = dataset+i
-        new_dataset = np.vstack((new_dataset, temp_array))
+        new_dataset = np.append(new_dataset, temp_array, axis=2)
 
+    print("dataset_mizumashi")
+    print(new_dataset.shape)
+    new_dataset = np.transpose(new_dataset, (2, 0, 1))
+    print(new_dataset.shape)
     return new_dataset
 
 def train (data_loader, model, criterion, optimizer, epoch):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
-
+    train_loss_list = []
     for i in range(epoch):
+        train_loss = 0
+        print("epoch:", i+1)
 
-      train_loss = 0
-      print("epoch:", i)
-
-      for j, (input_data, target) in enumerate(data_loader):
+        for j, (input_data, target) in enumerate(data_loader):
 
             model.zero_grad()
 
-            hidden = torch.zeros(1,10, 5)
+            hidden = torch.zeros(1, 1080, 5)
             output, hidden = model(input_data[:,:-1].float(), hidden.float())
             loss = criterion(output, target[:,1:].float())
             loss.backward()
             optimizer.step()
 
-            print(j, ":", loss.item())
+            print(j+1, ":", loss.item())
 
             train_loss += loss.item()
-            ave_train_loss = train_loss/len(data_loader.dataset)
-            train_loss_list.append(ave_train_loss)
-            print("ave:" ,ave_train_loss)
-            print()
+        ave_train_loss = train_loss/len(data_loader.dataset)
+        train_loss_list.append(ave_train_loss)
+        print("ave:" ,ave_train_loss)
+        print()
 
 
 def test(test_points, model):
 
     record_output = np.array([0])
 
-    for i in range(100):
-        hidden = torch.zeros(20, 20)
-        test_output, hidden = model(test_points.float(), hidden.float())
+    for i in range(1080):
+        hidden = torch.zeros(1, 1080, 5)
+        test_output, hidden = model(input_data[:,:-1].float(), hidden.float())
         test_points = test_points[1:20]
         test_poits.append(test_output)
         test_output = test_output.reshape(1,2).detach().numpy()
@@ -124,7 +128,7 @@ def main():
     batch_size  = 10 # 1つのミニバッチのデータの数
     epoch = 100  # epoch数
 
-    input_size = 2
+    input_size = 1
     hidden_size = 5
 
     seq_num = 100
@@ -138,7 +142,7 @@ def main():
     dataset = my_dataset(points)
 
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=6)
-
+    print("dataloader type", type(data_loader))
     """
     print("test points:", test_points.shape)
     print(test_points)
